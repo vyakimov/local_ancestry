824ef502040f3fedb5190416f0a68635b4843ed1
viya <viya@s-genepi1-p.ssi.ad>
2016-12-20 16:30:20 +0100

    improved wrapper to handle all phenos

--- gemma_wrapper.sh
+++ gemma_wrapper.sh
@@ -1,15 +1,30 @@
-name=$1 ##name as header in /data/INGO/NMR_2015sep09/headers.txt
-ptgo="../intermediate_data/gemma_out/${name}/"
+name=$1 ##name as header in /data/INGO/NMR_2015sep09/headers_clean.txt
+headers="/data/INGO/NMR_2015sep09/headers_clean_column.txt"
+function werk(){
+	if [ -z $1 ]; then
+		echo "needs a pheno name"
+	else
+		name="$1"
+		ptgo="../intermediate_data/gemma_out/${name}/"
+		echo working on $name
+		Rscript make_gemma_phenotypes.R ${name}
+		seq 1 22 | parallel ./run_gemma.sh {} ${name}
+		head -1 ${ptgo}chr1.assoc.txt > ${ptgo}${name}.assoc.txt
+		for i in `seq 1 22` 
+		do
+			tail -n +2 -q ${ptgo}chr${i}.assoc.txt >> ${ptgo}${name}.assoc.txt
+		done
+		# rm ${ptgo}chr*assoc*
+		python manhattan-plot.py --image ../plots/manhattan/${name}.manhattan.pdf ${ptgo}${name}.assoc.txt
+		python make_qq_plt.py ${ptgo}${name}.assoc.txt ../plots/qq/${name}.qq.png
+	fi
+}
 
-Rscript make_gemma_phenotypes.R ${name}
-seq 1 22 | parallel ./run_gemma.sh {} ${name}
-head -1 ${ptgo}chr1.assoc.txt > ${ptgo}${name}.assoc.txt
-for i in `seq 1 22` 
-do
-	tail -n +2 -q ${ptgo}chr${i}.assoc.txt >> ${ptgo}${name}.assoc.txt
-done
-# rm ${ptgo}chr*assoc*
-
-python manhattan-plot.py --image ${ptgo}${name}.manhattan.png ${ptgo}${name}.assoc.txt
-python make_qq_plt.py ${ptgo}${name}.assoc.txt ${ptgo}${name}.qq.png
-
+echo working on $name
+if [ $name = "allofthephenos" ]; then
+	while IFS=$'\n\r \t' read -r line; do
+		werk "$line"
+	done < $headers
+else
+	werk $name
+fi
